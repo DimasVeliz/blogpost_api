d80e2ad1addba5ba7c6869681912cb60c6c5339d
DimasVeliz <trevor90094@yandex.com>
2023-04-20 19:52:38 +0200

    Project Scaffolding Added

---
+++ src/main/java/com/boosting/code/blogpost_api/BlogpostApiApplication.java
@@ -0,0 +1,13 @@
+package com.boosting.code.blogpost_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boosting/code/blogpost_api/BlogpostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boosting.code.blogpost_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
